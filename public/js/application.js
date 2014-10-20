d4bfe09a4267ab8f1b59bc97335c05422457ef4f
Stuart Pearlman <stupearlman@gmail.com>
2014-10-19 20:52:33 -0500

    Auto commit: Sat Oct 18 21:00:42 CDT 2014

--- public/js/application.js
+++ public/js/application.js
@@ -18,11 +18,20 @@ $(function() {
     });
 
     function perfectFit() {
+        
         if ($(window).width() < 500) {
-            $("a.navbar-brand").text("Tracker")
+            
+            $("a.navbar-brand").replaceWith("<a class='navbar-brand' href='/''><i>Tracker</i></a>");
+            $('#back').css('margin-right', "0em");
+            $("#phone-number,#pin").css("font-size", "1.0em");
+        
         } else {
+            
             $("a.navbar-brand").replaceWith("<a class='navbar-brand' href='/''>RightOn<i>Tracker</i></a>");
-        }
+            $('#back').css('margin-right', ".8em");
+            $("#phone-number,#pin").css("font-size", "1.5em");
+
+        };
 
         $(".align").css('height', ($(document).height()).toString());
     };
